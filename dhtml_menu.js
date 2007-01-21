883eb5353a13096e6d147f83f0bc68ae62fa15ce
Steve McKenzie <steve_mckenzie@45890.no-reply.drupal.org>
2007-01-21 09:03:12 +0000

    coding standards, ability to toggle on slide effects, and a few bug fixes

--- dhtml_menu.js
+++ dhtml_menu.js
@@ -3,14 +3,12 @@
 Drupal.dhtmlMenu = {};
 
 /**
-* Attaches the online users autoupdate behaviour to the block content.
-*/
+ * Attaches the online users autoupdate behaviour to the block content.
+ */
 Drupal.dhtmlMenu.autoAttach = function() {
-//  $('span.submenu').hide();
-
   var cookievalue = Drupal.dhtmlMenu.getCookie('dhtml_menu');
-  if (cookievalue != "") {
-    var cookieList = cookievalue.split(",");
+  if (cookievalue != '') {
+    var cookieList = cookievalue.split(',');
     for (var i = 0; i < cookieList.length; i++) {
       $('#'+ cookieList[i]).show();
       $('#menu-' + cookieList[i]).addClass('expanded');
@@ -18,63 +16,84 @@ Drupal.dhtmlMenu.autoAttach = function() {
   }
 
   $('ul.menu li > a').each(function() {
-    this.style.display = 'block';
-    this.style.paddingLeft = '2em';
-    this.style.marginLeft = '-2em';
-    this.style.zIndex = 2;
-    $(this).click(function(e) {
-       id = $(this).parents()[0].id.replace('menu-', '');
-       Drupal.dhtmlMenu.switchMenu($('#'+ id)[0], $(this).parents()[0]);
-       return false;
+    $(this)
+    .css({display: 'block', paddingLeft: '2em', marginLeft: '-2em', zIndex: 2})
+    .click(function(e) {
+      if ($(this).parent().children('div.submenu').length > 0) {
+        id = $(this).parents()[0].id.replace('menu-', '');
+        Drupal.dhtmlMenu.switchMenu($('#'+ id)[0], $(this).parents()[0]);
+        
+        return false;
+      } else {
+        window.location = this.href;
+      }
     })
     .dblclick(function(e) {
       window.location = this.href;
     });
   });
-
   $(window).unload(Drupal.dhtmlMenu.saveMenuState);
 };
 
+/**
+ *  Changes the state of a submenu from open to close.
+ */
 Drupal.dhtmlMenu.switchMenu = function(submenu, parent) {
   if($(parent).is('.expanded')) {
-    $(submenu).css('display', 'none');
+    if (Drupal.settings.dhtmlMenu.useEffects) {
+      $(submenu).slideUp('fast');
+    } else {
+      $(submenu).css('display', 'none');
+    }
     $(parent).removeClass('expanded').addClass('collapsed');
   } else {
-    $(submenu).css('display', 'block');
+    if (Drupal.settings.dhtmlMenu.useEffects) {
+      $(submenu).slideDown('fast');
+    } else {
+      $(submenu).css('display', 'block');
+    }
     $(parent).removeClass('collapsed').addClass('expanded');
   }
   Drupal.dhtmlMenu.saveMenuState();
 }
 
-Drupal.dhtmlMenu.getCookie = function(Name) {
-  var search = Name + "="
-  var returnvalue = "";
+/**
+ * Grabs the cookie data.
+ */
+Drupal.dhtmlMenu.getCookie = function(name) {
+  var search = name + '=';
+  var returnvalue = '';
+  
   if (document.cookie.length > 0) {
-    offset = document.cookie.indexOf(search)
+    offset = document.cookie.indexOf(search);
     if (offset != -1) {
-      offset += search.length
-      end = document.cookie.indexOf(";", offset);
-      if (end == -1) end = document.cookie.length;
-      returnvalue = unescape(document.cookie.substring(offset, end))
+      offset += search.length;
+      var end = document.cookie.indexOf(';', offset);
+      if (end == -1) {
+        end = document.cookie.length;
+      }
+      returnvalue = unescape(document.cookie.substring(offset, end));
     }
   }
 
   return returnvalue;
 }
 
-
+/**
+ * Saves the states of the menus.
+ */
 Drupal.dhtmlMenu.saveMenuState = function() {
-  var blocks = "";
-  $('span.submenu').each(function(i) {
-    if (this.style.display != "none") {
-      if (blocks != "") {
-        blocks += ",";
+  var blocks = '';
+  $('div.submenu').each(function(i) {
+    if (this.style.display != 'none') {
+      if (blocks != '') {
+        blocks += ',';
       }
       blocks += this.id;
     }
   });
 
-  document.cookie = "dhtml_menu=" + blocks + ";path=/" ;
+  document.cookie = 'dhtml_menu=' + blocks + ';path=/';
 }
 
 if (Drupal.jsEnabled) {
