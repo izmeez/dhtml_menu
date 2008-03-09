7cc9c2947375f9aa73039f473112d6c2d3c5984f
Bruno Massa <brmassa@67164.no-reply.drupal.org>
2008-03-09 00:32:04 +0000

    Misc; * Much better Doxygen docummentation * JS reviewed, including documentation, code organization and using the latest jQuery code

--- dhtml_menu.js
+++ dhtml_menu.js
@@ -1,76 +1,116 @@
-// $Id$
+/**
+ * $Id$
+ * @author: Bruno Massa http://drupal.org/user/67164
+ * @file slideshow_creator.js
+ * The main Javacript for this module
+ */
 
 Drupal.dhtmlMenu = {};
 
 /**
- * Attaches the online users autoupdate behaviour to the block content.
+ * Initialize the module's JS functions
  */
-Drupal.dhtmlMenu.autoAttach = function() {
-  var cookievalue = Drupal.dhtmlMenu.getCookie('dhtml_menu');
+Drupal.behaviors.dhtmlMenu = function(context) {
+  // Get the Cookie's data
+  var cookievalue = Drupal.dhtmlMenu.getCookie();
   if (cookievalue != '') {
     var cookieList = cookievalue.split(',');
     for (var i = 0; i < cookieList.length; i++) {
       $('#'+ cookieList[i]).show();
-      $('#menu-' + cookieList[i]).addClass('expanded');
+
+      $('menu-'+cookieList[i]).removeClass('collapsed').addClass('expanded');
+      $(cookieList[i]).css('display', 'block');
     }
   }
 
+  // Add jQuery effects to all menu items
   $('ul.menu li[@class!="leaf"] > a').each(function() {
     if ($(this).parent().children('div.submenu').length > 0) {
       $(this)
       .css({display: 'block', zIndex: 2})
-      .click(function(e) {
-        id = $(this).parents()[0].id.replace('menu-', '');
-        Drupal.dhtmlMenu.switchMenu($('#'+ id)[0], $(this).parents()[0]);
-          
-        return false;
-      })
       .dblclick(function(e) {
         window.location = this.href;
+      })
+      .click(function(e) {
+        id = '#' + $(this).parents()[0].id.replace('menu-', '');
+        Drupal.dhtmlMenu.switchMenu(id, $(this).parents()[0]);
+        return false;
       });
     }
   });
 
+  // Avoid using the saveMenuState function by mistake
   $(window).unload(Drupal.dhtmlMenu.saveMenuState);
-};
+}
 
 /**
- *  Changes the state of a submenu from open to close.
+ * Changes the state of a submenu from open to close.
+ *
+ * @param submenu
+ *   String. The submenu ID (including the "#")
+ * @param parent_menu
+ *   String. The parent item ID (including the "#")
  */
-Drupal.dhtmlMenu.switchMenu = function(submenu, parent) {
-  if($(parent).is('.expanded')) {
-    if (Drupal.settings.dhtmlMenu.useEffects) {
+Drupal.dhtmlMenu.switchMenu = function(submenu, parent_menu) {
+  // First, see if the menu is already expanded or collapsed,
+  // and perform the opposing effect
+  if($(parent_menu).is('.expanded')) {
+
+    // If the user wants the Fading effects, use it,
+    // otherwise, simply make the menu disapear
+    if (Drupal.settings.dhtmlMenu_useEffects) {
       $(submenu).slideUp('fast');
-    } else {
+    }
+    else {
       $(submenu).css('display', 'none');
     }
-    $(parent).removeClass('expanded').addClass('collapsed');
-  } else {
-    if (Drupal.settings.dhtmlMenu.useEffects) {
+
+    // Set the parent menu item as collapsed
+    $(parent_menu).removeClass('expanded').addClass('collapsed');
+  }
+  else {
+    if (Drupal.settings.dhtmlMenu_useEffects) {
       $(submenu).slideDown('fast');
-    } else {
+    }
+    else {
       $(submenu).css('display', 'block');
     }
-    $(parent).removeClass('collapsed').addClass('expanded');
+    $(parent_menu).removeClass('collapsed').addClass('expanded');
   }
+
+  // After all changes, save the current state
+  // of the menus, so other pages will load and use
+  // this new layout
   Drupal.dhtmlMenu.saveMenuState();
 }
 
 /**
  * Grabs the cookie data.
+ *
+ * @return
+ *   String. A list, separated by comma, of all menu IDs
+ *   that should be expanded.
  */
-Drupal.dhtmlMenu.getCookie = function(name) {
-  var search = name + '=';
+Drupal.dhtmlMenu.getCookie = function() {
+  var string = 'dhtml_menu=';
   var returnvalue = '';
-  
+
+  // If there is a previous instance of this cookie
   if (document.cookie.length > 0) {
-    offset = document.cookie.indexOf(search);
+
+    // Get the number of characters that have the list of values
+    // from 'dhtml_menu' index.
+    offset = document.cookie.indexOf(string);
+
+    // If its positive, there is a list!
     if (offset != -1) {
-      offset += search.length;
+      offset += string.length;
       var end = document.cookie.indexOf(';', offset);
       if (end == -1) {
         end = document.cookie.length;
       }
+
+      // Get a list of all values that are saved on 'dhtml_menu='
       returnvalue = unescape(document.cookie.substring(offset, end));
     }
   }
@@ -82,19 +122,18 @@ Drupal.dhtmlMenu.getCookie = function(name) {
  * Saves the states of the menus.
  */
 Drupal.dhtmlMenu.saveMenuState = function() {
-  var blocks = '';
+  var menus = '';
+
+  // Get a list of menu IDs, saparated by comma
   $('div.submenu').each(function(i) {
     if (this.style.display != 'none') {
-      if (blocks != '') {
-        blocks += ',';
+      if (menus != '') {
+        menus += ',';
       }
-      blocks += this.id;
+      menus += this.id;
     }
   });
 
-  document.cookie = 'dhtml_menu=' + blocks + ';path=/';
-}
-
-if (Drupal.jsEnabled) {
-  $(document).ready(Drupal.dhtmlMenu.autoAttach);
+  // Save this values on the cookie
+  document.cookie = 'dhtml_menu=' + menus + ';path=/';
 }
