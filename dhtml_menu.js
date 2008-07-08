3aaf0295f0cdf0b33f9349263c079c5fde22458b
Christoph Burschka <arancaytar@28680.no-reply.drupal.org>
2008-07-08 13:44:51 +0000

    #277063 by Arancaytar: Rewrite of DHTML Menu, making the module completely independent of blocks.

--- dhtml_menu.js
+++ dhtml_menu.js
@@ -1,170 +1,126 @@
-/**
- * $Id$
- * @author: Bruno Massa http://drupal.org/user/67164
- * @file slideshow_creator.js
- * The main Javacript for this module
- */
+// $Id$
 
+/**
+ * @file dhtml_menu.js
+ * The Javascript code for DHTML Menu
+ */
+ 
 Drupal.dhtmlMenu = {};
-var dhtmlRun = false;
 
 /**
  * Initialize the module's JS functions
  */
-Drupal.behaviors.dhtmlMenu = function(context) {
+Drupal.behaviors.dhtmlMenu = function() {
+  var dhtmlRun;
+  
   // Do not run this function more than once.
-  if (dhtmlRun) return;
-  else dhtmlRun = true;
-  // Get the Cookie's data
-  var cookievalue = Drupal.dhtmlMenu.cookieGet();
-
-  // If there is any menus that should be expanded,
-  // do it now
-  if (cookievalue != '') {
-    var cookieList = cookievalue.split(',');
-    for (var i = 0; i < cookieList.length; i++) {
-      if (!document.getElementById(cookieList[i])) {
-        cookieList.splice(i, 1);
-      }
-      else {
-        submenu = document.getElementById(cookieList[i]);
-        menu = document.getElementById('menu-' + cookieList[i]);
-        $(menu).removeClass('collapsed').addClass('expanded');
-        $(submenu).removeClass('sub-collapsed').addClass('sub-expanded');
-        $(submenu).show();
-        $(submenu).css('display', 'block');
-      }
-    }
-  }
-
-  // Add jQuery effects (click and double click) to all menu items
-  $('ul.menu li[@class!="leaf"] > a').each(function() {
-    if ($(this).siblings('div.submenu').length > 0) {
-      $(this)
-      .css({display: 'block', zIndex: 2})
-      .dblclick(function(e) {
-        window.location = this.href;
-      })
-      .click(function(e) {
-        id = $(this).parents()[0].id.replace('menu-', '');
-        Drupal.dhtmlMenu.switchMenu(id, $(this).parents()[0]);
-        return false;
-      });
-    }
-  });
-}
-
-/**
- * Grabs the cookie data.
- *
- * @return
- *   String. A list, separated by comma, of all menu IDs
- *   that should be expanded.
- */
-Drupal.dhtmlMenu.cookieGet = function() {
-  var string = 'dhtml_menu=';
-  var returnvalue = '';
-
-  // If there is a previous instance of this cookie
-  if (document.cookie.length > 0) {
-
-    // Get the number of characters that have the list of values
-    // from 'dhtml_menu' index.
-    offset = document.cookie.indexOf(string);
-
-    // If its positive, there is a list!
-    if (offset != -1) {
-      offset += string.length;
-      var end = document.cookie.indexOf(';', offset);
-      if (end == -1) {
-        end = document.cookie.length;
-      }
-
-      // Get a list of all values that are saved on 'dhtml_menu='
-      returnvalue = unescape(document.cookie.substring(offset, end));
-    }
-  }
-
-  return returnvalue;
-}
-
-/**
- * Saves the states of the menus.
- */
-Drupal.dhtmlMenu.cookieSet = function() {
-  var menus = '';
-
-  // Get a list of menu IDs, saparated by comma
-  $('div.submenu').each(function(i) {
-    if ($(this).hasClass('sub-expanded')) {
-      if (menus != '') {
-        menus += ',';
-      }
-      menus += this.id;
-    }
-  });
-
-  // Save this values on the cookie
-  document.cookie = 'dhtml_menu=' + escape(menus) + ';path=/';
-}
-
-/**
-* Hide all submenus
-*/
-Drupal.dhtmlMenu.hideSiblings = function(parent_menu) {
-  $(parent_menu).siblings('.expanded').each(function() {
-    id = $(this).attr('id').replace('menu-', '');
-    Drupal.dhtmlMenu.switchMenu(id, this);
-  });
-};
-
-/**
- * Changes the state of a submenu from open to close.
- *
- * @param submenu
- *   String. The submenu ID (including the "#")
- * @param parent_menu
- *   String. The parent item ID (including the "#")
- */
-Drupal.dhtmlMenu.switchMenu = function(submenu, parent_menu) {
-  submenu = document.getElementById(submenu);
-
-  // First, see if the menu is already expanded or collapsed,
-  // and perform the opposing effect
-  if($(parent_menu).is('.expanded')) {
-
-    // If the user wants the Fading effects, use it,
-    // otherwise, simply make the menu disapear
-    if (Drupal.settings.dhtmlMenu_useEffects) {
-      $(submenu).hide('fast');
-    }
-    else {
-      $(submenu).css('display', 'none');
-    }
-
-    // Set the parent menu item as collapsed
-    $(parent_menu).removeClass('expanded').addClass('collapsed');
-    $(submenu).removeClass('sub-expanded').addClass('sub-collapsed');
+  if (dhtmlRun) {
+    return;
   }
   else {
-    if (Drupal.settings.dhtmlMenu_hideSiblings) {
-      Drupal.dhtmlMenu.hideSiblings(parent_menu);
-    }
-
-    // If the user wants the Fading effects, use it,
-    // otherwise, simply make the menu apear
-    if (Drupal.settings.dhtmlMenu_useEffects) {
-      $(submenu).show('fast');
-    }
-    else {
-      $(submenu).css('display', 'block');
-    }
-    $(parent_menu).removeClass('collapsed').addClass('expanded');
-    $(submenu).removeClass('sub-collapsed').addClass('sub-expanded');
+    dhtmlRun = true;
   }
 
-  // After all changes, save the current state
-  // of the menus, so other pages will load and use
-  // this new layout
+  // Get the settings.
+  var effects = Drupal.settings.dhtmlMenu;
+
+  // Get the cookie and mark the saved menus.
+  var cookie = Drupal.dhtmlMenu.cookieGet().split(',');
+  
+  for (i in cookie) {
+    $('#menu-' + cookie[i]).parents('li:first').addClass('dhtml-open');
+  }
+
+  // Close the menu items that were not saved as open.
+  $('li.expanded').not('.dhtml-open')
+  .removeClass('expanded').addClass('collapsed').children('ul').css('display', 'none');
+  Drupal.dhtmlMenu.cookieSet();
+
+  /* Add jQuery effects and listeners to all menu items.
+   * The ~ (sibling) selector is unidirectional and selects 
+   * only the latter element, so we must use siblings() to get 
+   * back to the link element.
+   */
+  $('ul.menu a ~ ul').siblings('a').each(function() {
+    if (effects.clone) {
+      $(this).clone().prependTo($(this).siblings('ul')).wrap('<li class="leaf fake-leaf"></li>');
+    }
+
+    if (effects.doubleclick) {
+      $(this).dblclick(function(e) {
+        window.location = this.href;
+      });
+    }
+
+    $(this).click(function(e) {
+      Drupal.dhtmlMenu.toggleMenu($(this).parent());
+      return false;
+    });
+  });
+}
+
+/**
+ * Toggles the menu's state between open and closed.
+ *
+ * @param li
+ *   Object. The <li> element that will be expanded or collapsed.
+ */
+Drupal.dhtmlMenu.toggleMenu = function(li) {
+  var effects = Drupal.settings.dhtmlMenu;
+
+  // If the menu is expanded, collapse it.
+  if($(li).hasClass('expanded')) {
+    if (effects.slide) {
+      $(li).children('ul').animate({height: 'hide', opacity: 'hide'}, '1000');
+    }
+    else $(li).children('ul').css('display', 'none');
+    $(li).removeClass('expanded').addClass('collapsed');
+  }
+  // Otherwise, expand it.
+  else {
+    if (effects.slide) {
+      $(li).children('ul').animate({height: 'show', opacity: 'show'}, '1000');
+    }
+    else $(li).children('ul').css('display', 'block');
+    $(li).removeClass('collapsed').addClass('expanded');
+
+    // If the effect is on, close all sibling menus.
+    if (effects.siblings) {
+      var id = $(li).children('a:first').attr('id');
+      siblings = $('ul.menu li.expanded').not(':has(#' + id + ')').filter(function() {
+        return !$(this).parents().is('#' + id);
+      });
+      if (effects.slide) {
+        $(siblings).children('ul').animate({height: 'hide', opacity: 'hide'}, '1000');
+      }
+      else $(siblings).children('ul').css('display', 'none');
+      $(siblings).removeClass('expanded').addClass('collapsed');
+    }
+  }
+
+  // Save the current state of the menus in the cookie.
   Drupal.dhtmlMenu.cookieSet();
 }
+
+/**
+ * Reads the dhtml_menu cookie.
+ */
+Drupal.dhtmlMenu.cookieGet = function() {
+  var c = /dhtml_menu=(.*?)(;|$)/.exec(document.cookie);
+  if (c) {
+    return c[1];
+  }
+  else return '';
+}
+
+/**
+ * Saves the dhtml_menu cooki.
+ */
+Drupal.dhtmlMenu.cookieSet = function() {
+  var expanded = new Array();
+  $('li.expanded').each(function() {
+    expanded.push($(this).children('a:first').attr('id').substr(5));
+  });
+  document.cookie = 'dhtml_menu=' + expanded.join(',') + ';path=/';
+}
+
